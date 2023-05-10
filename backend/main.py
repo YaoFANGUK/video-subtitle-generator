19491bc3815ee8a0402ee769970c4dfcb44d1e03
Yao Fang <flavioy@live.com>
2023-05-10 17:13:54 +0800

    修复切换配置的bug

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ from zhconv.zhconv import convert
 
 class AudioRecogniser:
     def __init__(self, language='auto'):
-        self.model_path = config.ASR_MODEL_PATH
+        self.model_path = config.get_model_path()
         self.model = whisper.load_model(self.model_path)
         self.language = language
 
